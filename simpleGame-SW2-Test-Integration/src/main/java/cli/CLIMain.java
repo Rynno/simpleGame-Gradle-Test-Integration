110a321ffc24327b1b7405f3023f1d134edd280e
rynno <ronny_4141@hotmail.com>
2016-01-12 11:29:44 -0500

    Stubbing - Mockito framework , Dependencies of a class

---
+++ simpleGame-SW2-Test-Integration/src/main/java/cli/CLIMain.java
@@ -0,0 +1,41 @@
+package cli;
+
+import java.util.Scanner;
+
+import core.Direction;
+import core.Game;
+import exception.OutOfBoardException;
+
+public class CLIMain {
+	
+	private static Game game;
+
+    public static void main(String[] args) {
+
+        game = new Game();
+        Scanner scanner = new Scanner(System.in);
+        while(!game.isGameOver()) {
+            System.out.println(game.toString());
+            int chosenint;
+            for (chosenint = -10;
+                    chosenint >= Direction.values().length
+                    || chosenint <0; chosenint=scanner.nextInt()) {
+                int counter = 0;
+                for(Direction d : Direction.values()) {
+                    System.out.println(counter + ": "+d.name());
+                    counter++;
+                }
+                System.out.println("Please chose a direction: ");
+            }
+            try {
+                System.out.println(game.moveNextPawn(
+                                       Direction.values()[chosenint]));
+            } catch (OutOfBoardException e) {
+                System.out.println("You can't go that way!");
+            }
+        }
+        scanner.close();
+
+    }
+
+}
---
+++ simpleGame-SW2-Test-Integration/src/main/java/core/Direction.java
@@ -0,0 +1,10 @@
+package core;
+
+/**
+ * The directions that can be taken by a moving Pawn.
+ * @author Erwan Bousse
+ *
+ */
+public enum Direction {
+    Up,Down,Left,Right;
+}
